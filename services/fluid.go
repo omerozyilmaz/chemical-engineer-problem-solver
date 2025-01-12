c4eda2c5805c9d3a02816bc81feea1d1021944cd
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2025-01-12 17:44:26 +0300

    Update

--- services/fluid.go
+++ services/fluid.go
@@ -9,7 +9,7 @@ func BernoulliEquation(p1, v1, h1, p2, v2, h2, rho, g float64) float64 {
 	return p1 + 0.5*rho*math.Pow(v1, 2) + rho*g*h1 - (p2 + 0.5*rho*math.Pow(v2, 2) + rho*g*h2)
 }
 
-// Continuity Equation
+
 func ContinuityEquations(a1, v1, a2 float64) float64 {
 	return (a1 * v1) / a2
 }
