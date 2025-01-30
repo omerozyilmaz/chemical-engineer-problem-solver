520c0a9029818c70e812a7dc98cd707454bf1a38
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2025-01-30 19:38:06 +0300

    update

--- services/fluid.go
+++ services/fluid.go
@@ -4,14 +4,14 @@ import (
 	"math"
 )
 
-// Bernoulli's Equation
+// Bernoulli's Equationnn
 func BernoulliEquation(p1, v1, h1, p2, v2, h2, rho, g float64) float64 {
 	return p1 + 0.5*rho*math.Pow(v1, 2) + rho*g*h1 - (p2 + 0.5*rho*math.Pow(v2, 2) + rho*g*h2)
 }
 
 
 func ContinuityEquations(a1, v1, a2 float64) float64 {
-	return (a1 * v1) / a2
+	return (a1 * v1 ) / a2
 }
 
 // Darcy-Weisbach Equation
